48ffbf96a449d4b3679cc676e46ee92dae16414c
pulpanj <pulpanj@volny.cz>
2024-01-19 15:45:33 +0100

    correct function case

--- src/function1.jl
+++ src/function1.jl
@@ -1,12 +1,12 @@
 # Example of function inside ./src/
 """
-    Function1(x,y)
-This is an example of Docstring. This function receives two 
+    function1(x,y)
+This is an example of Docstring. This function receives two
 numbers x and y and returns the sum of the squares.
 ```math
-x^2 + y^2 
+x^2 + y^2
 ```
 """
-function Function1(x,y)
-    return x^2+y^2
+function function1(x, y)
+    return x^2 + y^2
 end
